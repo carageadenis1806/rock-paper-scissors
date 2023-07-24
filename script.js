673ee8b272eb3e6a7cc65a510a7664da4de648e3
Caragea Denis <caragea.denis1806@gmail.com>
2023-07-24 13:36:16 +0300

    restart again

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// Computer Choice 
+/* 
 
-// I need a function that picks one random number from a total of 3. Each number returns something else
 
-function getComputerChoice () {
-    let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice == 1) {
-        return 'Rock';
-    } else if (choice == 2) {
-        return 'Paper';
-    }else {
-        return 'Scissors';
-    }
 
-}
 
+
+
+
+ */
